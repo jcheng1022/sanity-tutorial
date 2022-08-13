33e2adf719f2d7c1b497f3ab82b767630cff76c5
jcheng1022 <jackycheng1022@gmail.com>
2022-08-13 13:42:00 -0400

    first

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div >
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <h2 class="text-5xl text-red-400">hi</h2>
+    </div>
+  )
+}
+
+export default App
